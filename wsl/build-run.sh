980bb70d7129c0e682e2c182b422b1200df32219
Paulo Suderio <paulo.suderio@gmail.com>
2023-12-22 12:13:27 -0300

    systemd start

--- wsl/build-run.sh
+++ wsl/build-run.sh
@@ -13,6 +13,16 @@ sudo docker run -it \
   -e XDG_RUNTIME_DIR=$XDG_RUNTIME_DIR \
   -e PULSE_SERVER=$PULSE_SERVER \
   --gpus all \
+  --privileged \
+  --entrypoint=/usr/lib/systemd/systemd \
+  --env container=docker \
+  --mount type=bind,source=/sys/fs/cgroup,target=/sys/fs/cgroup \
+  --mount type=bind,source=/sys/fs/fuse,target=/sys/fs/fuse \
+  --mount type=tmpfs,destination=/tmp \
+  --mount type=tmpfs,destination=/run \
+  --mount type=tmpfs,destination=/run/lock \
+  --log-level=info \
+  --unit=sysinit.target \
   archwsl
 
   # docker export --output="./archwsl.tar" archwsl
